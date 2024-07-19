b9feafaf7ff7618429a6feb81e05a6adc6c62854
Adilet Toktybay <adilet.toktybay@gmail.com>
2024-07-19 15:09:40 +0500

    reroute root

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import './App.css'
-import {Route, Routes} from "react-router-dom";
+import {Navigate, Route, Routes} from "react-router-dom";
 import PullUps from "./pages/PullUps.jsx";
 import PushUps from "./pages/PushUps.jsx";
 import Nav from "./components/Nav.jsx";
@@ -9,9 +9,9 @@ function App() {
         <>
             <Nav />
             <Routes>
-                <Route path="/" element={<PullUps/>}/>
-                <Route path="/pull-ups" element={<PullUps/>}/>
-                <Route path="/push-ups" element={<PushUps/>}/>
+                <Route path='/' element={<Navigate to='/pull-ups' /> }/>
+                <Route path='/pull-ups' element={<PullUps/>}/>
+                <Route path='/push-ups' element={<PushUps/>}/>
             </Routes>
         </>
     )
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -3,8 +3,8 @@ import {NavLink} from "react-router-dom";
 function Nav () {
     return (
         <nav className='absolute bottom-6 left-1/2 transform -translate-x-1/2 flex space-x-4'>
-            <NavLink to="/pull-ups" className={({ isActive }) => isActive ? 'text-blue-500' : ''}>Pull-ups</NavLink>
-            <NavLink to="/push-ups" className={({ isActive }) => isActive ? 'text-blue-500' : ''}>Push-ups</NavLink>
+            <NavLink to='/pull-ups' className={({ isActive }) => isActive ? 'text-blue-500' : ''}>Pull-ups</NavLink>
+            <NavLink to='/push-ups' className={({ isActive }) => isActive ? 'text-blue-500' : ''}>Push-ups</NavLink>
         </nav>
     )
 }
