2d44a366a928f2c7e81861d19ae354d46c83aa84
Adilet Toktybay <adilet.toktybay@gmail.com>
2024-07-22 16:32:03 +0500

    navigation

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,14 +1,26 @@
-import {NavLink} from "react-router-dom";
+import {NavLink, useLocation, useNavigate} from "react-router-dom";
 import {Exercises} from "../utility/Exercises.js";
 import {MdImportExport} from "react-icons/md";
 
 function Nav () {
+    const navigate = useNavigate()
+    const location = useLocation()
+
+    function handleNavigate(event) {
+        let route = event.target.value
+        if (location.pathname !== '/' + route){
+            navigate(route)
+        }
+    }
+
     return (
         <nav className="fixed bottom-6 left-1/2 transform -translate-x-1/2 flex space-x-4 dark:bg-neutral-900 bg-white p-4 shadow-md">
-            {Exercises.map((exercise, key) => (
-                <NavLink key={key} to={exercise.route} className={({isActive}) => isActive ? 'text-blue-500' : ''}>
-                    {exercise.label}
-                </NavLink>))}
+            <select onClick={handleNavigate}>
+                {Exercises.map((exercise, key) => (
+                    <option key={key} value={exercise.route} selected={'/' + exercise.route === location.pathname}>
+                        {exercise.label}
+                    </option>))}
+            </select>
             <NavLink to='/download' className={({isActive}) => isActive ? 'text-blue-500 my-auto' : 'my-auto'}>
                 <MdImportExport/>
             </NavLink>
