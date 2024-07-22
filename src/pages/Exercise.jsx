d933aa3023358097a8c26e400da551ad60b2a56f
Adilet Toktybay <adilet.toktybay@gmail.com>
2024-07-22 15:51:13 +0500

    remove old format

--- src/pages/Exercise.jsx
+++ src/pages/Exercise.jsx
@@ -1,13 +1,13 @@
 import {useParams} from "react-router-dom";
 import Sets from "../components/Sets.jsx";
-import {convertToCamelCase, Exercises} from "../utility/Exercises.js";
+import {Exercises} from "../utility/Exercises.js";
 
 function Exercise() {
     const {exercise} = useParams()
 
     return (
         Exercises.findIndex(row => row.route === exercise) !== -1 ?
-            (<Sets key={exercise} exercise={convertToCamelCase(exercise)} />) : ''
+            (<Sets key={exercise} exercise={exercise} />) : ''
     )
 }
 
--- src/utility/Exercises.js
+++ src/utility/Exercises.js
@@ -2,10 +2,3 @@ export const Exercises = [
     {route: 'pull-ups', label: 'Pull-ups'},
     {route: 'push-ups', label: 'Push-ups'}
 ]
-
-export function convertToCamelCase(exercise) {
-    return exercise
-        .split('-')
-        .map(word => word.charAt(0).toUpperCase() + word.slice(1))
-        .join('');
-}
