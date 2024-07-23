75dc09721f43d08e0e0e19615940d911d99e3881
Adilet Toktybay <adilet.toktybay@gmail.com>
2024-07-23 11:25:36 +0500

    navigation

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -13,9 +13,18 @@ function Nav () {
         }
     }
 
+    function handleNavigateBack(event) {
+        let route = event.target.value
+        let exercise = Exercises.findIndex(exercise => '/' + exercise.route === location.pathname)
+        if (exercise === -1) {
+            navigate(route)
+        }
+    }
+
     return (
         <nav className="fixed bottom-6 left-1/2 transform -translate-x-1/2 flex space-x-4 dark:bg-neutral-900 bg-white p-4 shadow-md">
-            <select value={location.pathname.replace('/', '')} onChange={handleNavigate}>
+            <select value={location.pathname.replace('/', '')}
+                    onChange={handleNavigate} onClick={handleNavigateBack}>
                 {Exercises.map((exercise, key) => (
                     <option key={key} value={exercise.route}>
                         {exercise.label}
